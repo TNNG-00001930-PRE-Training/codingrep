b7fb8eb91772e8324e00c57d6721c2d277832461
srishiva3 <srisiva20112000>
2024-03-20 08:15:23 +0530

    Arithmetic operator

--- hello/src/main/app.py
+++
@@ -1,9 +0,0 @@
-# Application.py
-from lab import Lab
-
-if __name__ == "__main__":
-
-    hello = Lab()
-    output = hello.sayHello()
-
-    print(output)
--- hello/src/main/lab.py
+++
@@ -1,8 +0,0 @@
-# Lab.py
-class Lab:
-    """
-    This class contains a method sayHello() which returns "Your turn!".
-    """
-    def sayHello(self):
-        
-        return "Hello, World!"
